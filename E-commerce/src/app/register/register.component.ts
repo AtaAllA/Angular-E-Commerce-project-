b8bc4b4d5f134fbf4a9e4b6f28f31fadc417ee8f
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2023-02-09 21:23:12 +0200

    Angular lab 4 login & Register & environments for E commerce

--- E-commerce/src/app/register/register.component.ts
+++ E-commerce/src/app/register/register.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  userForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          // Validators.pattern(),
+        ],
+      ],
+      email: [
+        '',
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+      ],
+      username: [
+        '',
+        [
+          Validators.required,
+           Validators.pattern('/^(s+S+s*)*(?!s).*$/'),
+        ],
+      ],
+      password: [
+        '',
+        Validators.required,
+        Validators.pattern(
+          "'(?:[^A-Z]*[A-Z])[^!@#$&*]*[!@#$&*](?:[^0-9]*[0-9]){4}'"
+        ),
+      ],
+    });
+  }
+  callngform(){
+console.log(this.userForm);
+  }
 
+  ngOnInit(): void {}
 }
